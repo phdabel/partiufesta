6384bdcc0b072df651848406d4bd4585abb0efd4
Abel <abel@cpd.ufrgs.br>
2018-03-28 15:10:03 -0300

    - Strings were inserted in the strings.xml file.

--- app/src/main/java/br/com/partiufesta/partiufesta/MainActivity.java
+++ app/src/main/java/br/com/partiufesta/partiufesta/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
+        // @TODO Inserir referência ao fotógrafo - https://www.pexels.com/@maumascaro
     }
 }
