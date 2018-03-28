7d2410b3e6224cc85f0d8777daef7bcd80f2c7e3
Abel <phd.abel@gmail.com>
2018-03-28 01:38:34 -0300

    first commit

---
+++ app/src/main/java/br/com/partiufesta/partiufesta/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.partiufesta.partiufesta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+}
